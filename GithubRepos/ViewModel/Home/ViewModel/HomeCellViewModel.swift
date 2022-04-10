37fdfb5da3e51535d34884eaeec6d41e5db0c35c
eslamoomohamed <eslamomohamed@yahoo.com>
2022-04-10 17:54:39 +0200

    adding home viewModels

---
+++ GithubRepos/ViewModel/Home/ViewModel/HomeCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeCellViewModel.swift
+//  GithubRepos
+//
+//  Created by eslam mohamed on 10/04/2022.
+//
+
+import Foundation
+
+struct HomeCellViewModel {
+    let imageUrl:String
+    let repoTitle:String
+    let repoDecription:String
+    
+}
---
+++ GithubRepos/ViewModel/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,93 @@
+//
+//  HomeViewModel.swift
+//  GithubRepos
+//
+//  Created by eslam mohamed on 10/04/2022.
+//
+
+import Foundation
+
+class HomeViewModel:NSObject{
+    
+    
+    let networkShared = NetworkManager.shared
+    
+    private var cellViewModels:[HomeCellViewModel] = [HomeCellViewModel](){
+        didSet{
+            self.reloadTableViewClosure()
+        }
+    }
+    
+    private var errorMessage:ErrorMessages!{
+        didSet{
+            self.showErrorMessageClosure()
+        }
+    }
+    
+    var state:State!{
+        didSet{
+            switch state{
+            case .loading:
+                self.showLoadingToView()
+            case .finished:
+                self.hideLoadingToView()
+
+            default:
+                self.showLoadingToView()
+            }
+        }
+    }
+    
+    
+    
+    var reloadTableViewClosure:()->() = {}
+    var showEmptyStateClosure:(()->()) = {}
+    var showAlertMessageClosure: (()->()) = {}
+    var showErrorMessageClosure: (()->()) = {}
+    var showLoadingToView:  (()->()) = {}
+    var hideLoadingToView: (()->())  = {}
+    
+    
+    override init() {
+        super.init()
+        self.state = .empty
+        self.fetchData()
+    }
+    
+    
+    func fetchData(){
+        networkShared.fetchDataFromApi(urlString: URLs.repos(), baseModel: RepoBase.self) { result in
+            self.state = .finished
+            switch result{
+            case .success(let repoBase):
+                guard let items = repoBase.items else{return}
+                print(items.count)
+                self.processFetchedRepos(repos: items)
+            case.failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func getCellViewModel(at indexPath: IndexPath)->HomeCellViewModel{
+        return cellViewModels[indexPath.row]
+    }
+    
+    func createCellViewModel(item: Items)->HomeCellViewModel{
+        let cellViewModel = HomeCellViewModel(imageUrl: item.owner?.avatar_url ?? "", repoTitle: item.name ?? "", repoDecription: item.description ?? "")
+        
+        return cellViewModel
+    }
+    
+    func processFetchedRepos(repos:[Items]){
+        
+        var cellVM = [HomeCellViewModel]()
+        for itme in repos {
+            cellVM.append(createCellViewModel(item: itme))
+        }
+        self.cellViewModels = cellVM
+        
+    }
+    
+    
+}
